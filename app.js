bbbb08a4f93bb4ad9f03fa078aef4689d9fdc2a1
PavanKumar06 <spavankumard@gmail.com>
2023-12-06 01:57:29 -0500

    Mongo client code

--- app.js
+++ app.js
@@ -5,15 +5,40 @@ import cors from "cors";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import "dotenv/config";
+import mongoose from "mongoose";
+// mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+import UserRoutes from "./users/routes.js";
+import session from "express-session";
+
+
 
 const app = express();
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+mongoose.connect(CONNECTION_STRING);
+
 app.use(
   cors({
     credentials: true,
     origin: process.env.FRONTEND_URL,
   })
 );
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+  };
+}
+app.use(session(sessionOptions));
+  
 app.use(express.json());
+UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
